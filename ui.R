660948e9289d1af434d3793865b8f0d34f45b894
HMORIE <126384789+HMORIE@users.noreply.github.com>
2023-05-24 00:30:37 +0300

    Update ui.R

--- ui.R
+++ ui.R
@@ -3,19 +3,23 @@ library(shiny)
 library(leaflet)
 library(dplyr)
 
-# Create a RShiny UI
+# Create a Shiny UI
 shinyUI(
-  fluidPage(padding=5,
-  titlePanel("Bike Sharing Demand Prediction App"), 
-  # Create a side-bar layout
-  sidebarLayout(
-    # Create a main panel to show cities on a leaflet map
-    mainPanel(
-      leafletOutput('city_bike_map', height = 600, width = 600)
-    ),
-    # Create a side bar to show detailed plots for a city
-    sidebarPanel(
-      selectInput("city_select", "Select a city:", choices = c("All", "Seoul", "New York", "Paris", "London", "Suzhou"))
-      # select drop down list to select city
-    ))
-))
+  fluidPage(padding = 5,
+            titlePanel("Bike Sharing Demand Prediction App"),
+            # Create a sidebar layout
+            sidebarLayout(
+              # Create a main panel to show cities on a leaflet map
+              mainPanel(
+                leafletOutput('city_bike_map', height = 600, width = 600)
+              ),
+              # Create a sidebar to show detailed plots for a city
+              sidebarPanel(
+                selectInput("city_select", "Select a city:", choices = c("All", "Seoul", "New York", "Paris", "London", "Suzhou")), # select drop-down list to select city and plot output
+                plotOutput("temp_line"),  # Add plot output with ID "temp_line"
+                plotOutput("bike_line", click = "plot_click"),  # Add plot output with ID "bike_line"
+                verbatimTextOutput("bike_date_output")
+              )
+            )
+  )
+)
