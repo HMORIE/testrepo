4eb5776c20738cab6cea3110e2ae1d5c230c34be
HMORIE <126384789+HMORIE@users.noreply.github.com>
2023-05-24 00:32:06 +0300

    Update server.R

--- server.R
+++ server.R
@@ -1,4 +1,4 @@
-# Install and import required libraries
+# load required libraries
 library(shiny)
 library(ggplot2)
 library(leaflet)
@@ -29,15 +29,9 @@ shinyServer(function(input, output) {
   color_levels <- colorFactor(c("green", "yellow", "red"),
                               levels = c("small", "medium", "large"))
   
-  # Create another data frame called `cities_max_bike` with each row containing city location info and max bike
-  # prediction for the city
-  cities_max_bike <- city_weather_bike_df %>%
-    group_by(CITY_ASCII, LNG, LAT ) %>%
-    summarize(max(BIKE_PREDICTION))
-  
   # Render output plots with an id defined in ui.R
   output$city_bike_map <- renderLeaflet({
-    leaflet(cities_max_bike) %>%
+    leaflet(city_weather_bike_df) %>%
       addCircleMarkers(
         lng = ~LNG,
         lat = ~LAT,
@@ -48,10 +42,10 @@ shinyServer(function(input, output) {
       )
   })
   
-  # Render a leaflet map with circle markers and popup weather LABEL for all five cities
+  # Render a leaflet map with circle markers and popup weather LABEL for all cities
   output$city_bike_map_all <- renderLeaflet({
-    if (input$city_selection == "All") {
-      leaflet(cities_bike_pred) %>%
+    if (input$city_select == "All") {
+      leaflet(city_weather_bike_df) %>%
         addCircleMarkers(
           lng = ~LNG,
           lat = ~LAT,
@@ -67,10 +61,67 @@ shinyServer(function(input, output) {
     }
   })
   
+  # Render temperature trend plot for all cities or a specific city
+  output$temperature_trend_plot <- renderPlot({
+    if (input$city_select == "All") {
+      # Plot temperature trend for all cities
+      ggplot(city_weather_bike_df, aes(x = DATE, y = TEMPERATURE)) +
+        geom_line() +
+        geom_point() +
+        geom_text(aes(label = TEMPERATURE), nudge_y = 1) +
+        labs(x = "Date", y = "Temperature") +
+        theme_minimal()
+    } else {
+      # Plot temperature trend for a specific city
+      selected_city <- input$city_select
+      city <- city_weather_bike_df %>%
+        filter(CITY_NAME == selected_city)
+      
+      ggplot(city, aes(x = DATE, y = TEMPERATURE)) +
+        geom_line() +
+        geom_point() +
+        geom_text(aes(label = TEMPERATURE), nudge_y = 1) +
+        labs(x = "Date", y = "Temperature") +
+        theme_minimal()
+    }
+  })
+  
+  # Render bike-sharing demand prediction trend plot for all cities or a specific city
+  output$bike_line <- renderPlot({
+    if (input$city_select == "All") {
+      # Plot bike-sharing demand prediction trend for all cities
+      ggplot(city_weather_bike_df, aes(x = FORECASTDATETIME, y = BIKE_PREDICTION)) +
+        geom_line() +
+        geom_point() +
+        geom_text(aes(label = BIKE_PREDICTION), nudge_y = 1) +
+        labs(x = "Forecast Datetime", y = "Bike-Sharing Demand Prediction") +
+        theme_minimal()
+    } else {
+      # Plot bike-sharing demand prediction trend for a specific city
+      selected_city <- input$city_select
+      city <- city_weather_bike_df %>%
+        filter(CITY_NAME == selected_city)
+      
+      ggplot(city, aes(x = FORECASTDATETIME, y = BIKE_PREDICTION)) +
+        geom_line() +
+        geom_point() +
+        geom_text(aes(label = BIKE_PREDICTION), nudge_y = 1) +
+        labs(x = "Forecast Datetime", y = "Bike-Sharing Demand Prediction") +
+        theme_minimal()
+    }
+  })
+  
+  # Text output for clicked x and y values in bike-sharing demand prediction trend plot
+  output$bike_date_output <- renderText({
+    clicked_x <- nearPoints(city_weather_bike_df, input$plot_click, xvar = "FORECASTDATETIME")$FORECASTDATETIME
+    clicked_y <- nearPoints(city_weather_bike_df, input$plot_click, yvar = "BIKE_PREDICTION")$BIKE_PREDICTION
+    paste("Clicked Datetime: ", clicked_x, "\nClicked Bike Prediction: ", clicked_y)
+  })
+  
   # Render a leaflet map with one marker on the map and a popup with DETAILED_LABEL displayed for a specific city
   output$city_bike_map_selected <- renderLeaflet({
-    if (input$city_selection != "All") {
-      selected_city <- input$city_selection
+    if (input$city_select != "All") {
+      selected_city <- input$city_select
       city <- city_weather_bike_df %>%
         filter(CITY_NAME == selected_city)
       
